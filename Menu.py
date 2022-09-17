73d8235b6b416f52d5e6c3fa6727d2583a1ab01a
OAlexGiraldo <alexgiraldocar@gmail.com>
2022-09-17 13:06:28 -0500

    NTS7 se crea archivo menu

---
+++ Menu.py
@@ -0,0 +1,17 @@
+print("MENU")
+'''1.SALUDAR'''
+'''2.DESPEDIR'''
+'''3.SALIR'''
+while(observador != 0):
+    observador=int(input("Digite una opcion: "))
+    if():
+        break
+    elif():
+        print("")
+    elif():
+        print("")
+    else:
+        print("D")
+    
+else:
+    print("termine")
